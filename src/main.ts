7b5a879aacac32d3be02f1fa3eed2cedda76acd0
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-06-21 13:28:11 +0530

    feat: add a game session

--- src/main.ts
+++ src/main.ts
@@ -10,16 +10,64 @@ import {Goblin} from "./enemies/Goblin"
 import {Monster} from "./enemies/Monster"
 import {Skeleton} from "./enemies/Skeleton"
 import {Warlock} from "./enemies/Warlock"
+import {Entity} from "./entities/Entity";
+import {Player} from "./entities/Player";
+import {Enemy} from "./entities/Enemy";
 
-// Create player instances
-const infantry = new Infantry('Conan');
-const mage = new Mage('Gandalf');
-const archer = new Archer('Robin');
-const cavalry = new Cavalry('Arthur');
+class GameSession {
+    private static instance: GameSession | null;
+    protected player: Player;
+    protected enemy: Enemy;
 
-// Create enemy instances
-const goblin = new Goblin(1);
-const dragon = new Dragon(1);
-const monster = new Monster(2)
-const skeleton = new Skeleton(3)
-const warlock = new Warlock(4)
+    private constructor(player: Player, enemy: Enemy) {
+        this.player = player;
+        this.enemy = enemy;
+    }
+
+    public static getInstance(player: Player, enemy: Enemy): GameSession {
+        if (!GameSession.instance) {
+            GameSession.instance = new GameSession(player, enemy);
+        }
+        return GameSession.instance;
+    }
+
+    start(): void {
+        console.log(`Starting game with ${this.player.name} vs ${this.enemy.name}`);
+        while (!this.player.isDefeated && !this.enemy.isDefeated) {
+            this.playerTurn();
+            if (!this.enemy.isDefeated) {
+                this.enemyTurn();
+            }
+        }
+        this.endGame();
+    }
+
+    private playerTurn(): void {
+        console.log(`Player's turn: ${this.player.name} attacks ${this.enemy.name}`);
+        this.player.attack(this.enemy);
+        this.enemy.getHealth();
+    }
+
+    private enemyTurn(): void {
+        console.log(`Enemy's turn: ${this.enemy.name} attacks ${this.player.name}`);
+        this.enemy.attack(this.player);
+        this.player.getHealth();
+    }
+
+    private endGame(): void {
+        if (this.player.isDefeated) {
+            console.log(`${this.player.name} has been defeated. ${this.enemy.name} wins!`);
+        } else {
+            console.log(`${this.enemy.name} has been defeated. ${this.player.name} wins!`);
+        }
+        // Reset the instance after the game ends
+        GameSession.instance = null;
+    }
+}
+
+// Example usage
+const player = new Archer('Player1'); // You can choose any player class: Archer, Cavalry, Infantry, Mage
+const enemy = new Goblin(3); // You can choose any enemy class: Dragon, Goblin, Monster, Skeleton, Warlock
+
+const gameSession = GameSession.getInstance(player, enemy);
+gameSession.start();
